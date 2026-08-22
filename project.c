c7747a3710ad3e7366bd71b1073c8df1a3839e00
matykam <matyka.m2019@gmail.com>
2026-08-22 15:01:05 +0300

    создание прослойки между task_struct и exe экосистемой

--- project.c
+++ project.c
@@ -1,15 +1,34 @@
+/*Эта программа работает как прослойка между 
+*подсистемой процессов ядра линукс и 
+*экосистемы exe процессов*/
+
 #include <stdio.h>
-#include <sched.h>
-const unsigned long startmaskid = 0x99ul;
-struct exeprocess
+#include "sched.h"
+const unsigned long startmaskid = 0x99999ul;
+const struct task_struct *start = &init_task;
+
+
+struct exeprocess 
+/* структура используется для изолированной работы с процессами в которых исполняются exe файлы,
+*с помощью этой структуры экосистема exe может обращаться 
+*только к процессам которые были запущены внутри нее*/
 {
-    struct mm_struct *mm;
+    struct task_struct *tasks;
     pid_t exeid;
     pid_t pid;
     pid_t tgid;
-    unsigned int flags;
     unsigned int exeflags;
+    /* exeflags: 00000001-read,00000010-write,00000011-r/w,10000000-systemprogramm */
+    struct exeprocess *next;
+    struct exeprocess *prev;
 };
+ struct exeprocess __lowexe__(){
+     struct exeprocess init;
+     init.exeid = init.pid + startmaskid;
+     init.exeflags = 128; /* flag-10000000*/
+
+ }
+ 
 
  void *__openprocexe__(int *addrprocexe){
     printf("%d \n ",*addrprocexe);
@@ -31,6 +50,5 @@ int main() {
       a = execall[0](addr);
       printf("%d \n",*a);
       struct exeprocess one;
-      one.pid = 5;
-      
+      one.pid = 8;
 }
